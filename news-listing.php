19d53dfbf694bb0c90580abf9cb4376f5d65cbae
Jomon Lobo <jomonthomaslobo@gmail.com>
2024-05-09 11:38:06 +0530

    text domain update

--- news-listing.php
+++ news-listing.php
@@ -7,7 +7,7 @@
  * Author URI: https://jomonthomaslobo.github.io/
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain: aidsys-latest-news
+ * Text Domain: aidsys-news-listing
  */
 
 // Exit if accessed directly
